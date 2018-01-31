0ab59499ae81d53b6b0acd051f81eae5bb465715
Unknown <kelly.n.jahn@gmail.com>
2018-01-31 12:48:01 -0800

    class work

--- ClassExercises_Chapter4_KellyJahn.m
+++ ClassExercises_Chapter4_KellyJahn.m
@@ -32,43 +32,49 @@ for i = 1:5
 end
 F
 
-G = 1:25;
-G = reshape(G,5,5)
+G = reshape(1:25,5,5)
 
 H = ones(8);
 H(:,2:2:end) = 0
 
 %1,3,5,7
 I = zeros(8);
-for iRow = 1:2:8
-    for jCol = 1:2:8
-        if iRow == jCol
-            I(iRow,jCol) = 1;
+for i = 1:2:8
+    for j = 1:2:8
+        if i == j
+            I(i,j) = 1;
         end
     end
 end
 I
 
+%Better way
+I = zeros(8);
+for i = 1:2:8;
+    I(i,i) = 1;
+end
+I
+
 J = zeros(5);
 for i=1:5
-        if i == 1
-            J(i,:) = 0:4;
-        elseif i > 1
-            J(i,:) = [0:4]+(i-1);
-        end
+    J(i,:) = [0:4]+(i-1);
 end
 J
 
 K=ones(5);
-for iRow=1:5
-    for jCol = 1:5
-        K(iRow,jCol) = iRow*jCol;
+for i=1:5
+    for j = 1:5
+        K(i,j) = i*j;
     end
 end
 K
 
-L = 1:25;
-L =reshape(L,5,5)'
+%Better way
+for i=1:5
+    K(i,:)=[1:5]*i;
+end
+
+L =reshape(1:25,5,5)'
 
 %% Q4.2: 3D Matrices
 
@@ -90,7 +96,7 @@ sub2ind([4,3],3,2)
 
 %% Q4.4: Logical operations
 
-x = 1;
+x = -9;
 y = 5;
 z=1;
 
@@ -105,16 +111,16 @@ end
  x<2 || x>pi
  
  %c)
- x>2 && y<4 || z == 0
+ (x>2 && y<4) || z == 0
 
 %% Q4.5: while loops
 
 %ceil rounds the elements of X to the nearest integers towards infinity
 
 count = 1;
-roll = ceil(randi(6,1,2));
+roll = randi(6,1,2)
 while sum(roll) > 2 %while not [1 1], so total would be greater than 2
     count = count+1; %if not snake eyes, add 1 to the count
-    roll = ceil(randi(6,1,2)); %roll again
+    roll = randi(6,1,2) %roll again
 end
 disp(['Snake eyes after ', num2str(count),' rolls.']);
